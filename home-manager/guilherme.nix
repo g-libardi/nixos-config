d3557d835e75e58489e5cea5ba3c6aac9d9f9413
g-libardi <guilhermelibardi@hotmail.com>
2024-08-30 18:17:20 -0400

    sync

--- home-manager/guilherme.nix
+++ home-manager/guilherme.nix
@@ -5,6 +5,15 @@
   home.homeDirectory = "/home/guilherme";
   home.stateVersion = "24.05";
 
+  home.packages = with pkgs; [
+    neovim
+    gh
+    vscode-fhs
+    steam
+    qutebrowser
+    ticktick
+  ];
+
   programs.git = {
     enable = true;
     userName = "g-libardi";
--- home-manager/shared.nix
+++ home-manager/shared.nix
@@ -8,26 +8,17 @@
   ];
 
   home.packages = with pkgs; [
-    neovim
-    gh
-    vscode-fhs
     foot
     swaybg
-    swayosd
     swaynotificationcenter
-    steam
-    qutebrowser
     nerdfonts
     tofi
-    ticktick
   ];
   
-
   # hyprland config
   wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.extraConfig = "
     source = ${config.home.homeDirectory}/nixos-config/home-manager/hypr/hyprland.conf
-    $U_NIX_CONFIG = ${builtins.getEnv "U_NIX_CONFIG"}
   ";
   programs.waybar.enable = true;  
 }
