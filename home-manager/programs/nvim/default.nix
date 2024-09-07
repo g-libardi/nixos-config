e77b847ac64bc597a711814195094d70860c7e48
g-libardi <guilhermelibardi@hotmail.com>
2024-09-07 19:28:52 -0400

    sync

--- home-manager/programs/nvim/default.nix
+++ home-manager/programs/nvim/default.nix
@@ -1,27 +1,31 @@
 { pkgs }:
+let
+    nvimPkg = pkgs.neovim-unwrapped.overrideAttrs (oldAttrs: {
+        pname = "neovim-unwrapped-custom";
+        version = "stable";  # Replace with the desired version
+        src = pkgs.fetchFromGitHub {
+            owner = "neovim";
+            repo = "neovim";
+            rev = "stable";  # Match the desired version here
+            sha256 = "sha256-OsHIacgorYnB/dPbzl1b6rYUzQdhTtsJYLsFLJxregk=";  # You will need to fill in the hash
+        };
+    });
+in
 pkgs.stdenv.mkDerivation rec {
     pname = "nvim";
-    version = "1.0.0";
+    version = "stable";
     name = "libardi-nvim-${version}";
 
     buildInputs = [
-        (pkgs.neovim.overrideAttrs (oldAttrs: {
-            version = "v0.10.1";  # Replace with the desired version
-            src = pkgs.fetchFromGitHub {
-                owner = "neovim";
-                repo = "neovim";
-                rev = "v0.10.1";  # Match the desired version here
-                sha256 = "sha256-OsHIacgorYnB/dPbzl1b6rYUzQdhTtsJYLsFLJxregk=";  # You will need to fill in the hash
-            };
-        }))
+        nvimPkg
         pkgs.python3
         pkgs.wget
         pkgs.unzip
         pkgs.curl
         pkgs.git
         pkgs.wl-clipboard
-        ];
-    # propagatedBuildInputs = [ pkgs.python3 pkgs.wget pkgs.unzip pkgs.cur ];
+    ];
+    propagatedBuildInputs = [ nvimPkg ];
 
     unpackPhase = ":";
 
@@ -29,7 +33,7 @@ pkgs.stdenv.mkDerivation rec {
 mkdir -p $out/bin
 cat <<EOF > $out/bin/nvim
 #!/bin/bash
-PATH=$PATH:${pkgs.wget}/bin:${pkgs.unzip}/bin:${pkgs.python3}:${pkgs.git}:${pkgs.curl}:${pkgs.wl-clipboard} exec ${pkgs.neovim}/bin/nvim
+PATH=$PATH:${pkgs.wget}/bin:${pkgs.unzip}/bin:${pkgs.python3}:${pkgs.git}:${pkgs.curl}:${pkgs.wl-clipboard} exec ${nvimPkg}/bin/nvim
 EOF
 chmod +x $out/bin/nvim
     ";
