5de7435ca555e44aaaf5cc676672db2232998e48
g-libardi <guilhermelibardi@hotmail.com>
2024-09-09 14:39:30 -0400

    sync

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -1,38 +1,41 @@
 { config, pkgs, ... }:
 
 {
-  # Install firefox.
-  programs.firefox.enable = true;
-  programs.kdeconnect.enable = true;
-  programs.dconf.enable = true;
-  programs.nix-ld.enable = true;
+    # Install firefox.
+    programs.firefox.enable = true;
+    programs.kdeconnect.enable = true;
+    programs.dconf.enable = true;
+    programs.nix-ld.enable = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    # Docker
+    virtualisation.docker.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim
-    gnome.nautilus
-    nwg-look
-    lxqt.lxqt-policykit
-    udiskie
-    networkmanagerapplet
-    wlogout
-    swww
-    clipse
-    wl-clipboard
-    librewolf
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
 
-  #  wget
-  ];
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+        vim
+        gnome.nautilus
+        nwg-look
+        lxqt.lxqt-policykit
+        udiskie
+        networkmanagerapplet
+        wlogout
+        swww
+        clipse
+        wl-clipboard
+        librewolf
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+        #  wget
+    ];
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 }
